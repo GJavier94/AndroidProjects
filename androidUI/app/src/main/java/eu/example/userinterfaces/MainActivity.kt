f0e1a83ff9cd4ece98c8a2348c50a64d9a3a860b
Javier Armenta <javarmgar@gmail.com>
2021-08-05 11:55:57 -0500

    project for handling UI's

---
+++ androidUI/app/src/main/java/eu/example/userinterfaces/MainActivity.kt
@@ -0,0 +1,11 @@
+git package eu.example.userinterfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
