f9ab59f5bf2a568cf901a4e9b10045761d98d87d
Javier Armenta <javarmgar@gmail.com>
2021-08-06 13:48:43 -0500

    Working with atributtes alignParente<side> layout_<positionrespectosibling> layout_align<side> 7

---
+++ androidUI/app/src/main/java/eu/example/userinterfaces/MainActivity.kt
@@ -0,0 +1,49 @@
+package eu.example.userinterfaces
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.core.view.get
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.activities_layouts,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            _ ->
+            val position = spinner.selectedItemPosition
+            val array:Array<String> = resources.getStringArray( R.array.activities_layouts)
+            val nameActivity:String = array[position]
+            Log.d(TAG,"""$position ${array[position]}""" )
+
+            val intent = Intent(applicationContext,Class.forName("eu.example.userinterfaces.$nameActivity"))
+
+            startActivity(intent)
+        }
+
+    }
+
+    companion object{
+        const val TAG = "TAG:MainActivity"
+    }
+}
