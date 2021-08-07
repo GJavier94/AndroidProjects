f63dd36bb904745c5f09622ad2c01c7ef2d63c2a
Javier Armenta <javarmgar@gmail.com>
2021-08-07 11:23:52 -0500

    UI component ToggleButton manipulating attributes checkedOff, checkedOn, is Checked and the event handler OnCheckedChangeListener

--- androidUI/app/src/main/java/eu/example/userinterfaces/RelativeLayoutActivity.kt
+++ androidUI/app/src/main/java/eu/example/userinterfaces/RelativeLayoutActivity.kt
@@ -2,11 +2,24 @@ package eu.example.userinterfaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ToggleButton
 
 class RelativeLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_layout)
+
+        val toggleButton = findViewById<ToggleButton>(R.id.toggleButton3)
+
+        //toggleButton handled by event onCheckedChangeListener
+        toggleButton.setOnCheckedChangeListener {
+                buttonView, isChecked ->
+            when(isChecked){
+                true -> buttonView.text = resources.getText(R.string.toogle_set)
+                else -> buttonView.text = resources.getText(R.string.toogle_unset)
+            }
+        }
+
     }
 
 }
