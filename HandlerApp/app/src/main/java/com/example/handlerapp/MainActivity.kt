90c1d9a612fc88de8d44300aad8c34033864844f
Javier Armenta <javarmgar@gmail.com>
2021-08-30 11:14:48 -0500

    Creating two threads that whose parent is mainThread Then implementing handlers to communicate between eacher other one handlers needs the looper of the other The looper belong to the class HandlerThread.

--- HandlerApp/app/src/main/java/com/example/handlerapp/MainActivity.kt
+++ HandlerApp/app/src/main/java/com/example/handlerapp/MainActivity.kt
@@ -60,6 +60,65 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+        //Creating a conversation
+
+        val c1 = object:HandlerThread("C1"){
+            var cont:Int  = 0
+
+            var handler1:Handler? = null
+        }
+
+        val c2 = object:HandlerThread("C2"){
+            var cont:Int  = 0
+
+            var handler2:Handler? = null
+        }
+        c2.start()
+
+        c1.handler1 = object:Handler(c2.looper){
+            override fun handleMessage(msg: Message) {
+                val  messages:Array<String> = arrayOf("hola soy C1", "como estas ? ", "me llamo javier", "debo irme", "cuidate")
+                super.handleMessage(msg)
+                var numberMessage:Int = msg.arg1
+                if(numberMessage < messages.size){
+                    Log.i(TAG, "Thread:${Thread.currentThread().name}")
+                    Log.i(TAG, "MSG: ${messages[numberMessage]}")
+
+                    c2.handler2?.sendMessage(obtainMessage().apply {
+                        arg1 =  numberMessage
+                    } )
+                }
+            }
+        }
+        c1.start()
+        c2.handler2 = object :Handler(c1.looper) {
+            override fun handleMessage(msg: Message) {
+                val  messages:Array<String> = arrayOf("hola soy C2", "muy bien y tu ", "hola javier yo me llamo pedro", "cuidate", )
+                super.handleMessage(msg)
+                var numberMessage:Int = msg.arg1
+                if(numberMessage < messages.size){
+                    Log.i(TAG, "Thread:${Thread.currentThread().name}")
+                    Log.i(TAG, "MSG: ${messages[numberMessage]}")
+
+                    c1.handler1?.sendMessage(obtainMessage().apply {
+                        arg1 =  ++numberMessage
+                    } )
+                }
+            }
+        }
+
+
+
+        var conti:Int = 0
+        findViewById<Button>(R.id.buttonSendMessage).setOnClickListener {
+            if(c1.handler1 != null){
+                c1.handler1?.apply {
+                    val msg = obtainMessage()
+                    sendMessage(msg.apply { arg1 = conti })
+                }
+            }
+        }
     }
     companion object{
         const val TAG = "MainActivityLog"
