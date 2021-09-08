f8893ef199f49762f7633400fafaa771c3f75d45
Javier Armenta <javarmgar@gmail.com>
2021-09-08 16:39:13 -0500

    /**      * By calling invalidateOptionsMenu() on the host activity the callbacks methods are called      * so that it recreates the menu in case      */

--- MenusApp/app/src/main/java/com/example/menusapp/FragmentTimeHour.kt
+++ MenusApp/app/src/main/java/com/example/menusapp/FragmentTimeHour.kt
@@ -13,6 +13,7 @@ class FragmentTimeHour : Fragment() {
     private lateinit var buttonShowSettings: Button
 
     var showSettings:Boolean = false
+    var showOptionTimer = true
 
     internal fun showSettingsEnable(){
         this.showSettings = true
@@ -22,12 +23,6 @@ class FragmentTimeHour : Fragment() {
     }
 
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        Log.i(TAG, "onCreateOptionsMenu")
-
-        inflater.inflate(R.menu.fragment_alarm_menu, menu)
-
-    }
 
 
     /**
@@ -41,15 +36,22 @@ class FragmentTimeHour : Fragment() {
      *
      */
     override fun onPrepareOptionsMenu(menu: Menu) {
+        Log.i(TAG, "onPrepareOptionsMenu")
         val item = menu.findItem(FragmentAlarm.ID_ITEM_PROGRAMMATICALLY)
         if(item == null ){
             menu.add(0 , FragmentAlarm.ID_ITEM_PROGRAMMATICALLY, 0 , "Item programatically" )
         }
         menu.findItem(R.id.option_settings).isVisible = this.showSettings
+        menu.findItem(R.id.option_timer).isVisible = this.showOptionTimer
     }
 
+    /**
+     * By calling invalidateOptionsMenu() on the host activity the callbacks methods are called
+     * so that it recreates the menu in case
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate")
         setHasOptionsMenu(true)
     }
 
@@ -58,14 +60,25 @@ class FragmentTimeHour : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Log.i(TAG, "onCreateView")
         return inflater.inflate(R.layout.fragment_time_hour, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.i(TAG, "onViewCreated")
         super.onViewCreated(view, savedInstanceState)
         buttonShowSettings = view.findViewById<Button>(R.id.button_show_settings)
         buttonShowSettings.setOnClickListener {
             this.showSettings = !this.showSettings
+            /**
+             * This works because the item  it is hidden in actions menu
+             * but what if the action is not hidden there like
+             * the other timer item in the menu item
+             */
+            this.showOptionTimer = !this.showOptionTimer
+            requireActivity().invalidateOptionsMenu()
+
+
         }
     }
 
