4410a7a8ed7b0e154e6b5d66ca345d589b8ff420
Javier Armenta <javarmgar@gmail.com>
2021-09-22 12:36:21 -0500

    IT is possible to bind the layout view to architecture components i.e -> bind layout view component to ViewModel             UI <____>      datasource
    For getters it is invisible binding class has register observers so that UI automatically updates
    for setters
        1) in case is a listener -> e.g. checkbox or radioButtons  i define a lambda function used by the listener to change set the value of the viewmodel attr sex with changeSex method
    -->

--- DataBindingApp/app/src/main/java/com/example/databindingapp/viewmodels/ViewModelMainActivity.kt
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/viewmodels/ViewModelMainActivity.kt
@@ -3,9 +3,13 @@ package com.example.databindingapp.viewmodels
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.databindingapp.MainActivity
 import com.example.databindingapp.R
+import com.example.databindingapp.Sex
 import com.example.databindingapp.databinding.ActivityMainBinding
 import com.example.databindingapp.models.User
 
@@ -16,9 +20,10 @@ class ViewModelMainActivity :ViewModel(){
 
     var mainActivity: MainActivity? = null
     var textViewText = "This is a text in viewModel"
-    var numberOfUsers:Int = 0
-    val userList = mutableListOf<User>()
-    var lastIndex = 0
+    var numberOfUsers:MutableLiveData<Int> = MutableLiveData(0)
+
+    val userList = MutableLiveData<MutableList<User>>(mutableListOf())
+    var lastIndex:MutableLiveData<Int> = MutableLiveData<Int>(0)
 
     var nameEditText = ""
     var surnameEditText = ""
@@ -26,6 +31,21 @@ class ViewModelMainActivity :ViewModel(){
     var ageEditText = ""
 
     var binding: ActivityMainBinding? = null
+    var sex:MutableLiveData<Int> = MutableLiveData(Sex.MALE)
+
+
+    fun changeSex(view: View,radioButtonId:Int){
+        Log.i(TAG, "view:$view, radioButtonId: $radioButtonId")
+        when(radioButtonId){
+            R.id.femaleSexRadioButton ->{
+                sex.value = Sex.FEMALE
+            }
+            R.id.maleSexRadioButton ->{
+                sex.value = Sex.MALE
+            }
+        }
+        Log.i(TAG, "you changed your sex to ${sex.value}")
+    }
 
 
     fun onClickSender(view: View){
@@ -39,8 +59,33 @@ class ViewModelMainActivity :ViewModel(){
         }
 
         Log.i(TAG, "onClickSender().. $nameEditText, $surnameEditText, $passwordEditText, $ageEditText")
-        userList.add(User(nameEditText, surnameEditText, passwordEditText, ageEditText.toInt()))
-        lastIndex++
+        if(userList.value != null ){
+
+            Log.i(TAG, "adding the user to the list of users...")
+            Toast.makeText(this@ViewModelMainActivity.mainActivity?.applicationContext,"adding the user to the list of users...",Toast.LENGTH_SHORT).show()
+
+            userList.value?.also {
+                userList ->
+                userList.add(User(nameEditText, surnameEditText, passwordEditText, ageEditText.toInt())).also {
+                    wasRetrieved ->
+                    if(wasRetrieved){
+                        lastIndex.value = userList.lastIndex
+                        numberOfUsers.value = numberOfUsers.value?.plus(1)
+                        Toast.makeText(this@ViewModelMainActivity.mainActivity?.applicationContext, "New user added", Toast.LENGTH_SHORT).show()
+                        Log.i(TAG, "New user added")
+                    }else{
+                        Toast.makeText(this@ViewModelMainActivity.mainActivity?.applicationContext, "Couldn't add a new user into the list of users", Toast.LENGTH_SHORT).show()
+
+                        Log.i(TAG, "Couldn't add a new user into the list of users")
+
+                    }
+                }
+            }
+        }else{
+            Log.i(TAG,"Couldn't retrieve list of users")
+            Toast.makeText(this.mainActivity?.applicationContext,"Couldn't retrieve list of users", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 
